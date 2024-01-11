fb71d2e81822df4ab50ce7b8da5b017a7b4d0918
Siska <siput.apriliani@gmail.com>
2024-01-11 22:26:48 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/siska_fb/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.siska_fb
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:simple_icons/simple_icons.dart';
+import 'dart:html' as html;
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('fashionstore'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              html.window.open('https://github.com/Siputsss/siska-flutter', 'siska-flutter');
+            },
+            icon: const Icon(SimpleIcons.github, color: SimpleIconColors.github),
+          ),
+        ],
+      ),
+      body: Container(),
+    );
+  }
+}
